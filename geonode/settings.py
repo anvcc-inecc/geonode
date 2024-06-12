8299d1fa3c46f40df63030d26cb74eef243431d5
onlymapsdotnet <161183058+onlymapsdotnet@users.noreply.github.com>
2024-06-12 12:35:37 -0600

    Update settings.py

--- geonode/settings.py
+++ geonode/settings.py
@@ -2338,8 +2338,6 @@ IMPORTER_HANDLERS = ast.literal_eval(
     'importer.handlers.kml.handler.KMLFileHandler',\
     'importer.handlers.csv.handler.CSVFileHandler',\
     'importer.handlers.geotiff.handler.GeoTiffFileHandler',\
-    'importer.handlers.xml.handler.XMLFileHandler',\
-    'importer.handlers.sld.handler.SLDFileHandler',\
 ]",
     )
 )
